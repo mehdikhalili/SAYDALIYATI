3ccc16875fb888503ebc031add6302de093049ee
ayaarif121 <ayaarif121@gmail.com>
2021-04-18 21:53:30 +0000

    First

---
+++ app/src/main/java/com/iao/saydaliyati/PharmaciesActivity.java
@@ -0,0 +1,21 @@
+package com.iao.saydaliyati;
+
+import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class PharmaciesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity);
+    }
+}
