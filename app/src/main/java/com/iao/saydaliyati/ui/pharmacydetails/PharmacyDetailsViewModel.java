cd8a24438aadc5568d9fe8cffaa81f30e227c3d7
Mehdi Khalili <mehdikhalili860@gmail.com>
2021-04-22 17:13:39 +0000

    Add Profile Activity

--- app/src/main/java/com/iao/saydaliyati/ui/pharmacydetails/PharmacyDetailsViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.iao.saydaliyati.ui.pharmacydetails;
-
-import androidx.lifecycle.ViewModel;
-
-public class PharmacyDetailsViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
---
+++ app/src/main/java/com/iao/saydaliyati/ui/profile/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.iao.saydaliyati.ui.profile;
+
+import android.os.Bundle;
+
+import com.iao.saydaliyati.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
