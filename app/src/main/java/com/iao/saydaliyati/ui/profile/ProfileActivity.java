e943e06e41204405c9faec722719bcb579e7547d
Mehdi Khalili <mehdikhalili860@gmail.com>
2021-04-22 22:24:08 +0000

    Make phone call when the user clicks on call button

--- app/src/main/java/com/iao/saydaliyati/ui/profile/ProfileActivity.java
+++ app/src/main/java/com/iao/saydaliyati/ui/profile/ProfileActivity.java
@@ -1,19 +1,28 @@
 package com.iao.saydaliyati.ui.profile;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.iao.saydaliyati.MainActivity;
 import com.iao.saydaliyati.R;
 import com.iao.saydaliyati.entity.Pharmacy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private static final int PHONE_CALL_PERMISSION_REQUEST_CODE = 500;
 
     TextView tv_name, tv_address, tv_owner, tv_email, tv_phone, tv_arrondissement;
     ImageButton ib_map, ib_call;
@@ -55,5 +64,33 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
+        ib_call.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("NewApi")
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(activity, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+                    makeCall();
+                } else {
+                    requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, PHONE_CALL_PERMISSION_REQUEST_CODE);
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PHONE_CALL_PERMISSION_REQUEST_CODE && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            makeCall();
+        } else {
+            Toast.makeText(this, "Call phone permission denied", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void makeCall() {
+        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + pharmacy.getPhone()));
+        startActivity(intent);
     }
 }
